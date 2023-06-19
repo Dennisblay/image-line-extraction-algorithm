14dc3f1e5625070cf2d7b49cd9e541bd2b333663
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-06-19 09:38:07 +0000

    Renamed to image_line_reader.py

--- image_line_reader.py
+++ image_line_reader.py
@@ -35,6 +35,12 @@ class OCR:
             b = b.split(' ')
             cv2.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 
+    def draw_bounding_box_for_lines(self, bounding_boxes_container):
+        image = self.image
+        h, w, c = image.shape
+        for b in bounding_boxes_container:
+            cv2.rectangle(image, (int(b[0]), h - int(b[1])), (int(b[2]), h - int(b[3])), (0, 255, 0), 2)
+
     # Get verbose data including boxes, confidences, line and page numbers
     def get_image_data(self):
         return pytesseract.image_to_data(self.image)
@@ -45,12 +51,6 @@ class OCR:
             for line in text_lines:
                 f.write(f"{line}\n")
 
-    def draw_bounding_box_for_lines(self, bounding_boxes_container):
-        image = self.image
-        h, w, c = image.shape
-        for b in bounding_boxes_container:
-            cv2.rectangle(image, (int(b[0]), h - int(b[1])), (int(b[2]), h - int(b[3])), (0, 255, 0), 2)
-
     def extract_lines(self):  # Returns bounding box of lines
         lines_first_bound = None  # To temporarily store the bounding box of the first character on a line
         lines_last_bound = None  # To temporarily store the bounding box of the first character on a line
@@ -59,25 +59,24 @@ class OCR:
         lines_bounding_box = []  # To store bounding box of each line
         bounding_box_container = self.get_bounding_box_container
         lines_container = self.get_lines_container
-        # try:
-        for line_index, line in enumerate(lines_container):
-            line_length = len(line)  # for line in lines
-            for char_index, char in enumerate(line):  # for character in line
-                if char.isspace() or not char:
-                    continue
-                if lines_first_bound is None:
-                    lines_first_bound = bounding_box_container[count]
-                if line_length == char_index + 1:
-                    lines_last_bound = bounding_box_container[count]
-                    lines_bounding_box.append(
-                        (*lines_first_bound[:2], *lines_last_bound[2:], f"line-{line_index}"))
-                count += 1
+        try:
+            for line_index, line in enumerate(lines_container):
+                line_length = len(line)  # for line in lines
+                for char_index, char in enumerate(line):  # for character in line
+                    if char.isspace() or not char:
+                        continue
+                    if lines_first_bound is None:
+                        lines_first_bound = bounding_box_container[count]
+                    if line_length == char_index + 1:
+                        lines_last_bound = bounding_box_container[count]
+                        lines_bounding_box.append(
+                            (*lines_first_bound[:2], *lines_last_bound[2:], f"line-{line_index}"))
+                    count += 1
 
-            lines_first_bound = None
+                lines_first_bound = None
 
-        # except IndexError:
-        #     pass
-        # print(count)
+        except IndexError:
+            pass
         return lines_bounding_box
 
     @property
